07a989b947c75e6e0e9a09a2bcba2aac5637d226
Renan Rodrigo <renanrb@ime.usp.br>
2018-07-26 16:04:13 -0300

    Adicionar exemplo de escrever em arquivo

--- arquivos.js
+++ arquivos.js
@@ -8,10 +8,23 @@ fs.readFile('teste.txt', function(erro, sucesso){
         console.log(erro);
     }
     else{
-        console.log(sucesso.toJSON());
+        console.log(sucesso.toString());
         console.log("o texto está na tela");
     }
 });
 
 console.log("função de leitura executada");
 
+let paraEscrever = process.argv[2];
+
+fs.appendFile('escrever.txt', `${paraEscrever}\r\n`, erro => {
+    if(erro){
+        console.log("Erro ao escrever o arquivo. O erro foi:");
+        console.log(erro);
+    }
+    else{
+        console.log("Arquivo escrito com sucesso");
+    }
+});
+
+console.log("função de escrita executada");
